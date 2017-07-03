bdd7133fec21de5318a9d537c3d83418f80e9337
Swapnil Gaikwad <swapnilg@thoughtworks.com>
2017-07-03 13:31:40 +0530

    Implemented number store

--- src/app/redux/store/NumberStore.js
+++ src/app/redux/store/NumberStore.js
@@ -1,4 +1,10 @@
-import { createStore } from 'redux'
+import {createStore} from "redux";
+import dispatcher from "../dispatcher/StoreDispatcher";
 
-const store = createStore();
+const store = createStore(dispatcher);
 
+store.subscribe(() => {
+    console.log('State change ', store.getState());
+});
+
+export default store;
--- src/test/redux/store/NumberStore.test.js
+++ src/test/redux/store/NumberStore.test.js
@@ -1,5 +1,9 @@
+import expect from "expect";
+import store from "../../../app/redux/store/NumberStore";
+
 describe("Number store", () => {
     it("Simple test", () => {
-        
+        let state = store.dispatch({type: "INCREMENT"});
+        expect(store.getState()).toBe(1);
     });
 });
