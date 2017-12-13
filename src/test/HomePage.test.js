217a6c1440ad7bef82ec91fad426e11d59e4d15a
Swapnil Gaikwad <swapnilg@thoughtworks.com>
2017-12-13 23:43:47 +0530

    Fixed failing unit tests

--- src/test/HomePage.test.js
+++ src/test/HomePage.test.js
@@ -1,21 +1,13 @@
 import React from "react";
 import expect from "expect";
 import {mount} from "enzyme";
-import {Navbar} from "react-bootstrap";
+import AppBar from 'material-ui/AppBar';
 
 import HomePage from "../app/component/PageHeader";
 
 describe("PageHeader", () => {
-    it("render header with website name as GeekBook", () => {
+    it("render header with AppBar component", () => {
         let homePage = mount(<HomePage/>);
-        expect(homePage.find(Navbar.Header).length).toBe(1);
-    });
-
-    it("have dropdown for list of articles", () => {
-        let homePage = mount(<HomePage/>);
-        expect(homePage.find(Navbar.Collapse).length).toBe(1);
-
-        let itemList = homePage.find('.glyphicon-list');
-        expect(itemList.length).toBe(1);
+        expect(homePage.find(AppBar).length).toBe(1);
     });
 });
